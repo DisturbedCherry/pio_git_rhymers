6037b74a716822a4893ab9b188362b0d7fd69112
DisturbedCherry <jakub.t.wislicki@gmail.com>
2024-06-02 23:22:09 +0200

    17) Generated documentation for RhymersDemo class

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,17 +3,33 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
+/**
+* The RhymersDemo class provides a demonstration of the functionality of different rhymers.
+* This class includes the main method to execute the demo and a helper method to test the rhymers.
+*/
 class RhymersDemo {
 
     private static final int COUNT_IN_LIMIT = 15;
     private static final int RHYMERS_LAST_INDEX = 3;
     private static final int UPPER_BOUND = 20;
 
+    /**
+     * The main method is the entry point of the application.
+     * It creates a factory for rhymers and tests their functionality.
+     *
+     * @param args the command line arguments (not used)
+     */
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
         testRhymers(factory);
     }
 
+    /**
+     * Tests the rhymers by populating them with data and printing their outputs.
+     * This method demonstrates the counting and output capabilities of various rhymers.
+     *
+     * @param factory the factory used to create rhymers
+     */
     private static void testRhymers(Rhymersfactory factory) {
         DefaultCountingOutRhymer[] rhymers = {
                 factory.getStandardRhymer(),
