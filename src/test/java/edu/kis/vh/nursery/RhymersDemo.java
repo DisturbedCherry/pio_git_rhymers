5c8dd48b676139c5af9623c60fec583c27129c1d
DisturbedCherry <jakub.t.wislicki@gmail.com>
2024-06-02 23:14:39 +0200

    13) Created static method in RhymersDemo in order to simplify the code

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -11,27 +11,36 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
-        DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
-        for (int i = 1; i < COUNT_IN_LIMIT; i++)
-            for (int j = 0; j < RHYMERS_LAST_INDEX; j++)
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(Rhymersfactory factory) {
+        DefaultCountingOutRhymer[] rhymers = {
+                factory.getStandardRhymer(),
+                factory.getFalseRhymer(),
+                factory.getFIFORhymer(),
+                factory.getHanoiRhymer()
+        };
+
+        for (int i = 1; i < COUNT_IN_LIMIT; i++) {
+            for (int j = 0; j < RHYMERS_LAST_INDEX; j++) {
                 rhymers[j].countIn(i);
-        
+            }
+        }
+
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < COUNT_IN_LIMIT; i++)
+        for (int i = 1; i < COUNT_IN_LIMIT; i++) {
             rhymers[RHYMERS_LAST_INDEX].countIn(rn.nextInt(UPPER_BOUND));
-        
+        }
+
         for (int i = 0; i < rhymers.length; i++) {
-            while (!rhymers[i].callCheck())
+            while (!rhymers[i].callCheck()) {
                 System.out.print(rhymers[i].countOut() + "  ");
+            }
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[RHYMERS_LAST_INDEX]).reportRejected());
-        
     }
-    
 }
